ef49321bb8381e4e801fee99337d70dade8cc2bb
jimbotronn <jimbotronn@gmail.com>
2021-07-14 03:01:34 -0400

    update

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -11,7 +11,7 @@ const Timeline = () => {
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
     date="2011 - present"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        {/*icon={<Work/>}*/}
+   
   >
     <h3 className="vertical-timeline-element-title">Creative Director</h3>
     <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -23,7 +23,8 @@ const Timeline = () => {
     className="vertical-timeline-element--work"
     date="2010 - 2011"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      {/*icon={<Work/>}*/}
+    
+    
   >
     <h3 className="vertical-timeline-element-title">Art Director</h3>
     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -35,7 +36,7 @@ const Timeline = () => {
     className="vertical-timeline-element--work"
     date="2008 - 2010"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      {/*icon={<Work/>}*/}
+   
   >
     <h3 className="vertical-timeline-element-title">Web Designer</h3>
     <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
@@ -47,7 +48,7 @@ const Timeline = () => {
     className="vertical-timeline-element--work"
     date="2006 - 2008"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-      {/*icon={<Work/>}*/}
+    
   >
     <h3 className="vertical-timeline-element-title">Web Designer</h3>
     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -59,7 +60,7 @@ const Timeline = () => {
     className="vertical-timeline-element--education"
     date="April 2013"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-      {/*icon={<Location/>}*/}
+ 
   >
     <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
     <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
@@ -71,7 +72,7 @@ const Timeline = () => {
     className="vertical-timeline-element--education"
     date="November 2012"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-     {/*icon={<Location/>}*/}
+
   >
     <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
     <h4 className="vertical-timeline-element-subtitle">Certification</h4>
@@ -83,7 +84,7 @@ const Timeline = () => {
     className="vertical-timeline-element--education"
     date="2002 - 2006"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-      {/*icon={<Location/>}*/}
+   
   >
     <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
     <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
@@ -93,7 +94,7 @@ const Timeline = () => {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-      {/*icon={<Location/>}*/}
+  
   />
 </VerticalTimeline>
   );
