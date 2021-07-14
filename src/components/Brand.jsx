88f7aeebebff95d1a8119ae26ca3a603c3d1dd2e
jimbotronn <jimbotronn@gmail.com>
2021-07-14 05:34:57 -0400

    Update

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -29,7 +29,7 @@ export default function SimpleSlider() {
           <img src="/resume/img/partners/1.png" alt="partners brand" />
           </div>
           <div className="itemName1">
-            <h6 style="color:#bb0000;">The Ohio State Universite</h6>
+            <h6>The Ohio State Universite</h6>
             <p>B.S. Computer Science and Engineering<br></br>2003-2008</p>
           </div>
       </li>
@@ -38,7 +38,7 @@ export default function SimpleSlider() {
           <img src="/resume/img/partners/2.png" alt="partners brand" />
           </div>
           <div className="itemName2">
-            <h6 style="color:#35297f;">Capital University</h6>
+            <h6>Capital University</h6>
             <p>Master of Business Administration (MBA)<br></br>2015-2017</p>
           </div>
       </li>
