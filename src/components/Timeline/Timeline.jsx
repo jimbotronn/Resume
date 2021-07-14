623cfd5063d4b21d84dc64ee60e46fd7295b7add
jimbotronn <jimbotronn@gmail.com>
2021-07-14 02:30:02 -0400

    update

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,8 +1,6 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-...
-
 <VerticalTimeline>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
