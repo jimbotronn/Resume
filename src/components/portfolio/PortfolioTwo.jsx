16dc585a02e66c3393ab09e5cb667f199ae103f3
jimbotronn <jimbotronn@gmail.com>
2021-07-15 15:08:26 -0400

    Update

--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -19,7 +19,7 @@ const PortfolioTwo = () => {
                 <div className="title_flex">
                   <div className="left">
                     <span>Technical Skills</span>
-                    <h3>Apps and Systems I used </h3></div>
+                    <h3> </h3></div>
                 </div>
               </div>
               {/* End shane_tm_title */}
