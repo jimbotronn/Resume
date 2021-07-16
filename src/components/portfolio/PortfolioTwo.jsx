c44065845e558bde9a4240e325ba2c9dc7416579
jimbotronn <jimbotronn@gmail.com>
2021-07-16 16:57:26 -0400

    commit

--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -35,28 +35,28 @@ const PortfolioTwo = () => {
                   <div className="portfolio_list">
                                          <TabPanel>
     
-      <ul><li>Adobe Acrobat, DreamWeaver, Photoshop, Illustrator, XD</li><li>AS400</li> <li>Asana</li><li>Azure Board</li> <li>BarTender</li> <li>CRM Systems (SalesForce, ServiceNow)</li><li>GHX</li><li>CSS</li><li>DataVolley</li><li>Google Analytics, Docs, Sheets, Slides, Form, Workspace</li><li>ERP Systems (PeopleSoft, Workday)</li><li>HTML</li><li>Invision</li><li>JSON</li><li>JavaScript</li><li>Liquid</li>
-      <li>Inventory Systems (Omnicell, Tecsys, CBORD)</li><li>JIRA</li><li>Kronos</li><li>LucideChart</li><li>Microsoft 365 (Words, Excel, Outlook, PowerPoint, Forms, Access, Teams, Planners, Sharepoint)</li><li>Microsoft Power Platform (PowerApps, PowerAutomate, PowerBI)</li>
-                      <li>Microsoft Visio, and Project</li> <li>Monday.com</li><li>Nuvia</li><li>POS Systems (Sequia, Squre, Micros, MyMicros)</li><li>ReactJS</li><li>REST API</li><li>SmartDraw</li><li>SQL Database & Queries</li><li>Tableau</li><li>XML</li><li>Velo and Wix Code</li>                  
+      <ul><li>Adobe Acrobat, DreamWeaver, Photoshop, Illustrator, XD</li><li>AS400</li> <li>Asana</li><li>Azure</li> <li>BarTender</li><li>C,C#,C++</li> <li>CRM/ITSM Systems (SalesForce, ServiceNow)</li><li>GHX</li><li>CSS</li><li>DataVolley</li><li>Google Analytics, Docs, Sheets, Slides, Form, Workspace</li><li>ERP Systems (PeopleSoft, Workday)</li><li>HTML</li><li>Invision</li><li>JSON</li><li>JavaScript</li><li>Liquid</li>
+      <li>Inventory Systems (Bluebin, Omnicell, Tecsys, CBORD)</li><li>Java</li><li>JIRA</li><li>Kronos</li><li>LucideChart</li><li>Microsoft 365 (Words, Excel, Outlook, PowerPoint, Forms, Access, Teams, Planners, Sharepoint)</li><li>Microsoft Power Platform (PowerApps, PowerAutomate, PowerBI)</li>
+                      <li>Microsoft Visio, and Project</li> <li>Monday.com</li><li>Nintex</li><li>Nuvia</li><li>POS Systems (Sequia, Squre, Micros, MyMicros)</li><li>ReactJS</li><li>REST API</li><li>SmartSheet</li><li>SmartDraw</li><li>SQL Database & Queries</li><li>Tableau</li><li>XML</li><li>Velo and Wix Code</li> <li>WordPress</li>                 
                      </ul>
                       </TabPanel>
                       {/* END ALL PORTFOLIO GALLERY */}
 
                       <TabPanel>
-                      <ul alt="Web"><li>Adobe DreamWeaver, PhotoShop, Illustrator, XD</li><li>CSS</li><li>Google Analytics</li><li>HTML</li><li>Invision</li><li>JavaScript</li><li>JSON</li><li>Liquid</li><li>ReactJS</li>
-      <li>REST API</li><li>XML</li><li>Velo and Wix Code</li></ul>
+                      <ul alt="Web"><li>Adobe DreamWeaver, PhotoShop, Illustrator, XD</li><li>C,C#,C++</li><li>CSS</li><li>Google Analytics</li><li>HTML</li><li>Invision</li><li>Java</li><li>JavaScript</li><li>JSON</li><li>Liquid</li><li>ReactJS</li>
+      <li>REST API</li><li>XML</li><li>Velo and Wix Code</li><li>WordPress</li></ul>
                       </TabPanel>
 
                       <TabPanel>
-                      <ul alt="Business"><li>AS400</li><li>Adobe Acrobat</li><li>Asana</li><li>Azure Board</li><li>CRM Systems (SalesForce, ServiceNow)</li><li>ERP Systems (PeopleSoft, Workday)</li><li>GHX</li><li>Google Docs, Sheets, Slides, and Form</li>
-                      <li>Google Workspace</li><li>Inventory Systems (Omnicell, Tecsys, CBORD)</li><li>JIRA</li><li>Kronos</li><li>Monday.com</li><li>Microsoft 365 (Words, Excel, Outlook, PowerPoint, Forms, Access, Teams, Planners, Sharepoint)</li>
-                      <li>Microsoft Power Platform (PowerApps, PowerAutomate)</li><li>Microsoft , Visio, and Project</li>
-                      <li>POS Systems (Sequia, Squre, Micros, MyMicros)</li>
+                      <ul alt="Business"><li>AS400</li><li>Adobe Acrobat</li><li>Asana</li><li>Azure</li><li>CRM Systems (SalesForce, ServiceNow)</li><li>ERP Systems (PeopleSoft, Workday)</li><li>GHX</li><li>Google Docs, Sheets, Slides, and Form</li>
+                      <li>Google Workspace</li><li>Inventory Systems (BlueBin, Omnicell, Tecsys, CBORD)</li><li>JIRA</li><li>Kronos</li><li>Monday.com</li><li>Microsoft 365 (Words, Excel, Outlook, PowerPoint, Forms, Access, Teams, Planners, Sharepoint)</li>
+                      <li>Microsoft Power Platform (PowerApps, PowerAutomate)</li><li>Microsoft , Visio, and Project</li><li>Nintex</li>
+                      <li>POS Systems (Sequia, Squre, Micros, MyMicros)</li><li>SmartSheet</li>
       </ul>
                       </TabPanel>
 
                       <TabPanel>
-                      <ul alt="Data"> <li>BarTender</li><li>DataVolley</li><li>Google Workspace</li><li>LucideChart</li><li>Microsoft Power BI</li><li>Nuvia</li><li>SQL Database & Queries</li> <li>SmartDraw</li><li>Tableau</li>
+                      <ul alt="Data"> <li>BarTender</li><li>DataVolley</li><li>Google Analytics</li><li>LucideChart</li><li>Microsoft Power BI</li><li>Nuvia</li><li>SQL Database & Queries</li> <li>SmartDraw</li><li>Tableau</li>
                                           </ul>
                       </TabPanel>
                                    {/* End tabpanel */}
