26b773890fa33290ae59aa8bf21548207a042039
jimbotronn <jimbotronn@gmail.com>
2021-07-21 13:21:29 -0400

    Update

--- src/components/skills/SkillsTwo.jsx
+++ src/components/skills/SkillsTwo.jsx
@@ -11,12 +11,15 @@ const SkillsTwo = () => {
               <Reveal effect="fadeInLeft">
                 <div className="left">
                   <div className="shane_tm_title">
-                    <h3>Modern Development Methodologies & Frameworks</h3>
+                    <h3>Technical competence with dependability and loyalty</h3>
                   </div>
                   <div className="text">
                     <p>
-                      I understand how to adopt development methodologies and
-                      frameworks to meet business needs and produce the best results
+                    Grounded in the fundamental of information technologies and 
+                    strengthen by strategic thinking, my background has demonstrated 
+                    my ability to excel in developing innovative solutions and to 
+                    provide leadership in establishing as well as executing technology 
+                    strategies, goals, and priorities aligned with enterprise mission.
                     </p>
                   </div>
                 </div>
@@ -27,8 +30,11 @@ const SkillsTwo = () => {
                 <div className="right">
                   <div className="tokyo_progress">
                     <div className="progress_inner">
+                    <div className="shane_tm_title">
+                  <span>Area of Emphasis</span>
+                </div>
                       <span>
-                        <span className="label">Agile      |      Waterfall      |      DevOps</span>
+                        <span className="label"> Solution architecture & Digital Transformation</span>
                         <span className="number"></span>
                       </span>
                       <div className="background">
@@ -44,7 +50,7 @@ const SkillsTwo = () => {
 
                     <div className="progress_inner">
                       <span>
-                        <span className="label">TOGAF        |        Zachman</span>
+                        <span className="label">Business & Technology Alignment</span>
                         <span className="number"></span>
                       </span>
                       <div className="background">
@@ -60,7 +66,7 @@ const SkillsTwo = () => {
 
                     <div className="progress_inner">
                       <span>
-                        <span className="label">IaaS     |     SaaS     |     PaaS Design</span>
+                        <span className="label">Change Management & Risk Assessmentn</span>
                         <span className="number"></span>
                       </span>
                       <div className="background">
